d724a45d2a287fe78c48aa156999bd61d1a8da02
ostapturash <oturash@kse.org.ua>
2023-10-30 15:57:27 +0200

    Average time endpoints implemented

--- main.py
+++ main.py
@@ -28,14 +28,43 @@ def total_online_time_for_user(user_id):
 
     return {"totalTime": 0}
 
+def average_online_time_for_user(user_id):
+
+    users_data = fetch_user_data(0)
+
+    for user in users_data:
+        if user.get('userId') == user_id:
+            last_seen = user.get('lastSeenDate', None)
+            if last_seen:
+                last_seen = last_seen.split('.')[0]
+                last_seen_date = datetime.strptime(last_seen, "%Y-%m-%dT%H:%M:%S")
+
+                current_time = datetime.now()
+                total_time_seconds = (current_time - last_seen_date).total_seconds()
+
+                weekly_average = total_time_seconds / (7 * 24 * 3600)
+                daily_average = total_time_seconds / (24 * 3600)
+
+                return {
+                    "weeklyAverage": weekly_average,
+                    "dailyAverage": daily_average
+                }
+    return{
+        "weeklyAverage": 0,
+        "dailyAverage": 0
+    }
+
+
 
 
 def main():
 
     user_id = input("Enter your user id: ")
 
-    result = total_online_time_for_user(user_id)
-    print(result)
+    total = total_online_time_for_user(user_id)
+    print(total)
+    average = average_online_time_for_user(user_id)
+    print(average)
 
 
 
